43468494ec794d2b382e70d966c6d3f79eb7de46
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2024-08-17 19:28:09 -0400

    se implementa la lectura del sensor DS18B20 de forma asincrona

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 
 //global variables
 enum LOG_LEVEL {
-  DEBUG = 1,
-  INFO = 2,
-  WARNING = 3,
-  ERROR = 4
+  DEBUG,
+  INFO,
+  WARNING,
+  ERROR,
 };
+
+//logger level definition.
+LOG_LEVEL logger_level = DEBUG;
 
 // pinout
 #define RADAR 26
@@ -23,18 +26,23 @@ enum LOG_LEVEL {
 #define AMB_TEMP 32
 #define NUMPIXELS 24
 
-float ambient_t_reading = 0.0;
+float ambient_temperature = 0.0;
 bool last_output_state = false;
+int sensor_resolution = 12; // bits
+unsigned long lastTempRequest = 0;
+unsigned long lastIOUpdate = 0;
+int delayInMillis = 0;
+const unsigned long IOUpdateInterval = 5L * 1000L; // 5 second.
+
 
 //oneWire and DallasTemperature
 OneWire ow_ambient_t(AMB_TEMP);
 DallasTemperature ambient_t_sensor(&ow_ambient_t);
 
+DeviceAddress ambientSensorAddress;
+
 //RTC
 RTC_DS3231 RTC;
-
-//logger level definition.
-LOG_LEVEL logger_level = DEBUG;
 
 //neopixel
 
@@ -50,7 +58,6 @@ void logger(char *message, LOG_LEVEL msg_level){
       Serial.print(now);
       Serial.print(" > msg: ");
       Serial.println(message);
-      Serial.flush();
     }
     break;
 
@@ -60,7 +67,6 @@ void logger(char *message, LOG_LEVEL msg_level){
       Serial.print(now);
       Serial.print(" > msg: ");
       Serial.println(message);
-      Serial.flush();
     }
     break;
 
@@ -70,7 +76,6 @@ void logger(char *message, LOG_LEVEL msg_level){
       Serial.print(now);
       Serial.print(" > msg: ");
       Serial.println(message);
-      Serial.flush();
     }
     break;
 
@@ -80,7 +85,6 @@ void logger(char *message, LOG_LEVEL msg_level){
       Serial.print(now);
       Serial.print(" > msg: ");
       Serial.println(message);
-      Serial.flush();
     }
     break;
   
@@ -93,50 +97,58 @@ void logger(char *message, LOG_LEVEL msg_level){
 }
 
 void get_ambient_temperature() {
-  char word_buffer[30];
+  char word_buffer[40];
   char temp_buffer[8];
   //--
-  logger("getting temperature from OneWire sensor..", DEBUG);
-  ambient_t_sensor.requestTemperaturesByIndex(0);
-  float tempC = ambient_t_sensor.getTempCByIndex(0); // temp of the first sensor.
-  if (tempC == -127)
-  {
-    logger("Error -127 while reading OneWire sensor; [Ambient temperature]", ERROR);
-  }
-  if (tempC == 85) {
-    logger("Got 85 deg. as tempC reading", WARNING);
-  }
 
-  //-- logging
-  dtostrf(tempC, 5, 2, temp_buffer);
-  sprintf(word_buffer, "Temperature value: %s", temp_buffer);
-  logger(word_buffer, DEBUG);
-  //--
+  if (millis() - lastTempRequest >= delayInMillis) {
+    // waited long enough?
+    logger("reading temperature from DS18B20 sensor..", DEBUG);
+    float tempC = ambient_t_sensor.getTempCByIndex(0); // temp of the first sensor.
+    if (tempC == -127)
+    {
+      logger("Error -127 while reading sensor; [Ambient temperature]", ERROR);
+    }
+
+    //-- logging
+    dtostrf(tempC, 5, 2, temp_buffer);
+    sprintf(word_buffer, "Temperature value: %s °C", temp_buffer);
+    logger(word_buffer, DEBUG);
+    //--
+
+    //request new readings.
+    ambient_t_sensor.requestTemperatures();
+    lastTempRequest = millis();
+  }
   return;
 }
 
 
-void update_inputs(){
-char word_buffer[40];
-sprintf(word_buffer, "input: RAD[%d] MANU[%d] AP[%d] NOW[%d]", 
-digitalRead(RADAR), digitalRead(MANUAL_BTN), digitalRead(AP_BTN), digitalRead(NOW_BTN));
-logger(word_buffer, DEBUG);
-return;
-}
-
-void update_outputs(){
-  //--
-  switch (last_output_state)
-  {
-  case true:
-    digitalWrite(NETWORK_LED, LOW);
-    last_output_state = false;
-    break;
+void update_IO(){
+  // io updates.
+  char word_buffer[40];
   
-  default:
-    digitalWrite(NETWORK_LED, HIGH);
-    last_output_state = true;
+  if (millis() - lastIOUpdate >= IOUpdateInterval) {
+    //-
+    sprintf(word_buffer, "inputs: RAD[%d] MAN[%d] AP[%d] NOW[%d]", 
+    digitalRead(RADAR), digitalRead(MANUAL_BTN), digitalRead(AP_BTN), digitalRead(NOW_BTN));
+    logger(word_buffer, DEBUG);
+    switch (last_output_state)
+    {
+    case true:
+      digitalWrite(NETWORK_LED, LOW);
+      last_output_state = false;
+      break;
+    
+    default:
+      digitalWrite(NETWORK_LED, HIGH);
+      last_output_state = true;
+      break;
+    }
+
+    lastIOUpdate = millis();
   }
+
   return;
 }
 
@@ -161,32 +173,33 @@ void setup() {
     RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
   logger("RTC configured!", DEBUG);
+
   //--DS18B20
-  logger("Setting up OneWire sensor with 12 bits res.", DEBUG);
+  logger("Setting up DS18B20 sensor.", DEBUG);
   ambient_t_sensor.begin();
-  ambient_t_sensor.setResolution(12); //12 bits resolution.
-  delay(500);
-  logger("first temp. reading..", DEBUG);
-  get_ambient_temperature();
+  ambient_t_sensor.getAddress(ambientSensorAddress, 0);
+  ambient_t_sensor.setResolution(ambientSensorAddress, sensor_resolution);
+  ambient_t_sensor.setWaitForConversion(false);
+  logger("request for DS18B20 temp. readings.", DEBUG);
+  ambient_t_sensor.requestTemperatures();
+  delayInMillis = 750 / (1 << (12 - sensor_resolution));
+  lastTempRequest = millis();
+  logger("DS18B20 settings done.", DEBUG);
+
   //-- I-O
+  logger("setting up I-O pins", DEBUG);
   pinMode(RADAR, INPUT);
   pinMode(MANUAL_BTN, INPUT);
   pinMode(AP_BTN, INPUT);
   pinMode(NOW_BTN, INPUT);
   pinMode(NETWORK_LED, OUTPUT);
 
-  //-- NeoPixel
-
-  logger("setup finished. 1sec", DEBUG);
-  delay(1000);
+  // finish.
+  logger("setup finished!", INFO);
 }
 
 void loop() {
   //get DS18B20 temperature and print to serial logger.
   get_ambient_temperature();
-  update_inputs();
-  update_outputs();
-
-  //--delay and back again.
-  delay(1000);
+  update_IO();
 }
